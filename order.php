264d6ebaa43e132aa747073518cd66b1a4ed5f71
obinar <obinar@yandex.ru>
2021-02-25 00:47:38 +0300

    dte

--- order.php
+++ order.php
@@ -3,7 +3,7 @@
 //	header('Location: http://google.com/');
 //	return;
 //	}
-if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) { $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP']; }
+1if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) { $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP']; }
 $ip = $_SERVER['REMOTE_ADDR'];
 $name = $_POST['name'];
 $phone = $_POST['phone'];
