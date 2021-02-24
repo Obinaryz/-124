86591dfde5cb512d5f6b2c54e163c6a1e22bde19
obinar <obinar@yandex.ru>
2021-02-25 00:46:43 +0300

    fcomm

--- order.php
+++ order.php
@@ -1,8 +1,8 @@
 <?php 
-if (isset($_COOKIE["NAHUI"])) {
-	header('Location: http://google.com/');
-	return;
-	}
+//if (isset($_COOKIE["NAHUI"])) {
+//	header('Location: http://google.com/');
+//	return;
+//	}
 if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) { $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP']; }
 $ip = $_SERVER['REMOTE_ADDR'];
 $name = $_POST['name'];
